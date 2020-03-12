b725f7470043d38487f471af4b728563d752a20b
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-12 19:37:38 -0400

    added server, updated npm dependecies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require('dotenv').config()
-// const mongoose = require("mongoose");
-// const routes = require("./routes");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const axios = require("axios");
@@ -14,24 +14,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/public"));
 }
 // Add routes, both API and view
-// app.use(routes);
-app.get('/api/test', function() {
-  console.log('registering beacon', process.env.API_KEY);
-  axios.post(
-    'https://proximitybeacon.googleapis.com/v1beta1/beacons:register?key=' + process.env.API_KEY,
-    {
-      "advertisedId": {
-        "type": "EDDYSTONE",
-        "id": "LyNEVPSRG6n/pgAAAAAAAw=="
-      },
-      "status": "ACTIVE"
-    }).then((res) => {
-     // console.log(res.body);
-    }).catch(err => console.log(err));
-})
+app.use(routes);
+
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/vhost");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/virtualhost");
 
 // Start the API server
 app.listen(PORT, function() {
