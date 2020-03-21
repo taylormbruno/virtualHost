c9140d9d9e6e4c4497c38927d16c22ca4f9b5edb
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-21 15:52:27 -0400

    added validation for email

--- client/src/components/Signup/validate.js
+++ client/src/components/Signup/validate.js
@@ -2,6 +2,8 @@ const reSpec = /^\w+$/;
 const reNum = /[0-9]/;
 const reLow = /[a-z]/;
 const reUp = /[A-Z]/;
+// eslint-disable-next-line
+const reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function validateForm(form) {
     // username
@@ -9,6 +11,7 @@ function validateForm(form) {
         alert("Error: Username must contain only letters, numbers and underscores!");
         return false;
     }
+
     // password
     if(form.pass1.length < 6) {
         alert("Error: Password must contain at least six characters!");
@@ -35,10 +38,19 @@ function validateForm(form) {
         alert("Error: Passwords do not match!");
         return false;
     }
+
+    // email
+    if (!reEmail.test((form.email).toLowerCase())) {
+        alert("Error: Please enter a valid email!");
+        return false;
+    }
+
+    // all passes
     else {
         alert(`Welcome ${form.first_name} ${form.last_name}`);
         return true;
     }
+    
 }
 
 export default validateForm;
