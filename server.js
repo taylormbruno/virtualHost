50003b6060b526165558a098cad5e0684c164c56
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-22 19:35:35 -0400

    updated vendorController to search for vendor name that includes str

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const passport = require('passport');
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize()); 
 
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
