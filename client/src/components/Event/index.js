01171d507c26319273e8d7ea88322225fdbc32b1
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-24 11:24:26 -0400

    fixed conflcit in client packagejson that kept client from compiling

--- client/src/components/Event/index.js
+++ client/src/components/Event/index.js
@@ -2,14 +2,12 @@ import React from "react";
 // import './style.css';
 // import VendorCard from '../Event/VendorCards/vendorCard';
 import SearchFilter from './VendorSearch/vendorSearch';
-import SideBarComponent from './eventSideBar';
 
 
 const EventPage = () => (    
     <>
         <SearchFilter/>        
         {/* <VendorCard/>   */}   
-        <SideBarComponent/>     
     </>
 )
 
