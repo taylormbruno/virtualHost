a53a1dc339fd44a1ea3b9635fb5eff8313898756
Jodi Rhoades <jo_kawgirl@yahoo.com>
2020-03-24 11:54:17 -0400

    deleting eventsidebar

--- client/src/components/Event/eventSideBar.js
+++ client/src/components/Event/eventSideBar.js
@@ -1,46 +1,30 @@
-import React, {Component} from "react";
+import React from "react";
 import Sidebar from "react-sidebar";
 
-
-const mql = window.matchMedia(`(min-width: 800px)`);
-
-class SideBarComponent extends Component {
+class SideBarComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarDocked: mql.matches,
-      sidebarOpen: false
+      sidebarOpen: true
     };
-
-    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
-
-  componentWillMount() {
-    mql.addListener(this.mediaQueryChanged);
-  }
-
-  componentWillUnmount() {
-    mql.removeListener(this.mediaQueryChanged);
   }
 
   onSetSidebarOpen(open) {
     this.setState({ sidebarOpen: open });
   }
 
-  mediaQueryChanged() {
-    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-  }
-
   render() {
     return (
-      <Sidebar
+      <Sidebar className="sidebar"
         sidebar={<b>Sidebar content</b>}
         open={this.state.sidebarOpen}
-        docked={this.state.sidebarDocked}
         onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "white" } }}
       >
-        <b>Main content</b>
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Click Here for This Events Details
+        </button>
       </Sidebar>
     );
   }
