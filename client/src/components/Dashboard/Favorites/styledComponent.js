6e2fa8ed675617d076166036c1ae6a0d774a8a33
Kacie Hatley <kaciehatley@gmail.com>
2020-03-25 16:42:56 -0400

    added favorites section to dashboard

---
+++ client/src/components/Dashboard/Favorites/styledComponent.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Table } from 'semantic-ui-react';
+
+
+export const StyledHeader = styled(Table.HeaderCell)`
+    &&& {  
+        background-color: #a9a6c9;
+        color: white;
+    }   
+    `;
