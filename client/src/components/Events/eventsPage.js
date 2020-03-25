13c3a306c02a9ed7fdff3a00c73b1d403dd318d8
Jodi Rhoades <jo_kawgirl@yahoo.com>
2020-03-24 22:23:30 -0400

    fixed respoonsive search bar in events page

--- client/src/components/Events/eventsPage.js
+++ client/src/components/Events/eventsPage.js
@@ -5,68 +5,65 @@ import './style.css'
 import EventSearchFilter from './EventSearch/eventSearch';
 
 const EventPage = () => (
-    <div>
-        <Grid.Column centered stackable >
-                <Segment id="column">
-                <Header id='eventHeader'>Events</Header>
-                <EventSearchFilter />                
-                    <div> 
-                        <Card.Group centered>  
-                         {/* <EventCard/> */}                
-                            <Card>
-                                <Placeholder.Header image/>
-                                <Card.Content>
-                                <Card.Header>Daniel</Card.Header>
-                                <Card.Meta>Joined in 2016</Card.Meta>
-                                <Card.Description>
-                                    Daniel is a comedian living in Nashville.
-                                </Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                <a>
-                                    <Icon name='user' />
-                                    10 Friends
-                                </a>
-                                </Card.Content>
-                            </Card>
-                            <Card>
-                                <Placeholder.Header image/>
-                                <Card.Content>
-                                <Card.Header>Daniel</Card.Header>
-                                <Card.Meta>Joined in 2016</Card.Meta>
-                                <Card.Description>
-                                    Daniel is a comedian living in Nashville.
-                                </Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                <a>
-                                    <Icon name='user' />
-                                    10 Friends
-                                </a>
-                                </Card.Content>
-                            </Card>
-                            <Card>
-                                <Placeholder.Header image/>
-                                <Card.Content>
-                                <Card.Header>Daniel</Card.Header>
-                                <Card.Meta>Joined in 2016</Card.Meta>
-                                <Card.Description>
-                                    Daniel is a comedian living in Nashville.
-                                </Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                <a>
-                                    <Icon name='user' />
-                                    10 Friends
-                                </a>
-                                </Card.Content>
-                            </Card>
-                        </Card.Group>
-                    </div>
-                </Segment>
-            </Grid.Column>     
-            
-</div>
+    <div>   
+        <Segment id="column">
+                    <Header id='eventHeader'>Events</Header>
+                        <EventSearchFilter/>                
+                        <div> 
+                            <Card.Group centered>  
+                            {/* <EventCard/> */}                
+                                <Card>
+                                    <Placeholder.Header image/>
+                                    <Card.Content>
+                                    <Card.Header>Daniel</Card.Header>
+                                    <Card.Meta>Joined in 2016</Card.Meta>
+                                    <Card.Description>
+                                        Daniel is a comedian living in Nashville.
+                                    </Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                    <a>
+                                        <Icon name='user' />
+                                        10 Friends
+                                    </a>
+                                    </Card.Content>
+                                </Card>
+                                <Card>
+                                    <Placeholder.Header image/>
+                                    <Card.Content>
+                                    <Card.Header>Daniel</Card.Header>
+                                    <Card.Meta>Joined in 2016</Card.Meta>
+                                    <Card.Description>
+                                        Daniel is a comedian living in Nashville.
+                                    </Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                    <a>
+                                        <Icon name='user' />
+                                        10 Friends
+                                    </a>
+                                    </Card.Content>
+                                </Card>
+                                <Card>
+                                    <Placeholder.Header image/>
+                                    <Card.Content>
+                                    <Card.Header>Daniel</Card.Header>
+                                    <Card.Meta>Joined in 2016</Card.Meta>
+                                    <Card.Description>
+                                        Daniel is a comedian living in Nashville.
+                                    </Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                    <a>
+                                        <Icon name='user' />
+                                        10 Friends
+                                    </a>
+                                    </Card.Content>
+                                </Card>
+                            </Card.Group>
+                        </div>
+                    </Segment>    
+    </div>
 )
 
 export default EventPage;
