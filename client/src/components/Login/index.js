23ec202fa189bed7b3b6033bc0fbfe302862faac
Taylor Bruno <taylorbruno895@gmail.com>
2020-03-29 15:10:31 -0400

    added base functions for user login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,38 +1,90 @@
-import React from 'react'
-import { Form, Grid, Image, Segment } from 'semantic-ui-react'
-import Logo from "./logo.png"
-import "./style.css"
-import { StyledButton } from "./styledComponents.js"
+import React from "react";
+import { Form, Grid, Image, Segment } from "semantic-ui-react";
+import Logo from "./logo.png";
+import "./style.css";
+import { StyledButton } from "./styledComponents.js";
+import API from '../../utils/API';
 
-const LoginForm = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' id="outerContainer">
-    <Grid.Column id="container">
-      <Form size='large'>
-        <Segment stacked>
-          <Image id="logo" src={Logo} />
-          <div id="formContainer">
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address'/>
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
-          </div>
-          <StyledButton fluid size='large' id="login">
-            Login
-          </StyledButton>
-                <StyledButton href="/signup" fluid size='large'>
-            Create Account
-        </StyledButton>
-        <StyledButton fluid size='large'>
-            Continue As Guest
-        </StyledButton>
-        </Segment>
-      </Form>
-    </Grid.Column>
-  </Grid>
-)
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default LoginForm
+  handleInputChange = (event) => {
+    if (event.target.name === "username") {
+      event.target.value = event.target.value.toLowerCase();
+    }
+    const { name, value } = event.target;
+    this.setState({...this.state, [name]: value });
+  };
+
+  signupSuccess = (id) => {
+    useHistory().push('/mydashboard/' + id);
+  };
+
+  handleFormSubmit = async (event) => {
+    event.preventDefault();
+      const newObj = {
+        username: this.state.username,
+        password: this.state.password,
+      }
+      let newUser = await API.loginUser(newObj);
+      if (newUser.status === 200) {
+        console.log(`Hello ${newUser.data.first_name} ${newUser.data.last_name}`);
+        this.loginSuccess(newUser.data._id);
+      }
+      else {
+        console.log(`Error ${newUser.status}: ${newUser.statusText}`)
+      }
+  };
+
+  render() {
+    return (
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+        id="outerContainer"
+      >
+        <Grid.Column id="container">
+          <Form size="large">
+            <Segment stacked>
+              <Image id="logo" src={Logo} />
+              <div id="formContainer">
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  name="username"
+                  onChange={this.handleInputChange}  
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  onChange={this.handleInputChange}  
+                />
+              </div>
+              <StyledButton fluid size="large" id="login" onClick={this.handleFormSubmit}>
+                Login
+              </StyledButton>
+              <StyledButton href="/signup" fluid size="large" >
+                Create Account
+              </StyledButton>
+              <StyledButton fluid size="large">
+                Continue As Guest
+              </StyledButton>
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default LoginForm;
