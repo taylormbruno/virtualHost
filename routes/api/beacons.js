f8685e6af2887fcf2f22464b6db77fd92423c489
Taylor Bruno <taylorbruno895@gmail.com>
2020-04-03 11:39:45 -0400

    fixed google sign in

--- routes/api/beacons.js
+++ routes/api/beacons.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const axios = require("axios");
-// matches /api/beacons/register
+// matches /api/beacons/ +
 
 router.post("/register", function(req, res) {
   console.log(req.body)
-  axios({
-    method: "post",
-    url: "https://proximitybeacon.googleapis.com/v1beta1/beacons:register?key=" + proccess.env.API_KEY,
-    data: {
-      advertisedId: { type: "EDDYSTONE", id: req.body.beaconID },
-      status: "ACTIVE",
-      expectedStability: "STABLE",
-      description: req.body.vendor_name,
-    }
-  }).then(response => {
-    console.log(response);
-  });
+  // axios({
+  //   method: "post",
+  //   url: "https://proximitybeacon.googleapis.com/v1beta1/beacons:register?key=" + proccess.env.API_KEY,
+  //   data: {
+  //     advertisedId: { type: "EDDYSTONE", id: req.body.beacon_id },
+  //     status: "ACTIVE",
+  //     expectedStability: "STABLE",
+  //     description: req.body.vendor_name,
+  //   }
+  // }).then(response => {
+  //   console.log(response);
+  // });
 });
 
 module.exports = router;
